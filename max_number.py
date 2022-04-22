53af022580f128a66315934962e2ee4d5d36d671
chhotu kumar <104151354+chhotuNG@users.noreply.github.com>
2022-04-22 15:51:51 +0530

    Add files via upload

---
+++ max_number.py
@@ -0,0 +1,35 @@
+#write a code find second max number 
+
+#a=int(input("enter 1number:-"))
+#b=int(input("enter 1number:-"))
+#c=int(input("enter 1number:-"))
+#d=int(input("enter 1number:-"))
+#if (b>a and c<a and d<a):
+#    print(a,"is second max")
+#elif (c>a and d<a and b<a):
+#    print(a,"is second max")
+#elif(d>a and c<a and b<a):
+#    print(a,"is second max")
+
+#elif(a>b and c<b and d<b):
+#       print(b,"is second max")
+#elif (c>b and a<b and d<b):
+#    print(b,"is second max")
+#elif (d>b and a<b and c<b):
+#            print(b,"is second max")
+#            
+#            
+#if (a>c and b<c and d<c):
+#     print(c,"is second max ")
+#elif (b>c and a<c and d<c):
+#     print(c,"is second max ")
+#elif (d>c and a<c and b<c):
+#            print(c,"is second max ")
+#            
+#elif(a>d and b<d and c<d):
+#      print (d,"is second max")
+#elif (b>d and a<d and c<d):
+#      print (d,"is second max")
+#elif (c>d and a<d and b<d):
+#            print (d,"is second max")
+
